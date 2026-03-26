9d4f5c23c2743e225b5d971df316aa50df76bb91
cewaki <cewaki77@gmail.com>
2026-03-27 01:32:14 +0300

    Add files via upload

---
+++ weather app/script.js
@@ -0,0 +1,7 @@
+// Example starter script
+document.addEventListener("DOMContentLoaded", () => {
+  const weatherDiv = document.getElementById("weather");
+
+  // Temporary message until we connect to an API
+  weatherDiv.textContent = "Weather data will appear here!";
+});
